11991f2872a0ecd9404e154c585768555369829f
dbs1597 <dbs1597@naver.com>
2018-06-18 18:38:57 +0900

    기본 틀

--- hangmanclient.c
+++ hangmanclient.c
@@ -1,1 +1,13 @@
-hang
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main(argc, *argc[]) {
+    
+    //socket
+    
+    //connet
+    
+    //read/write
+    
+    //close
+}
--- hangmanserver.c
+++ hangmanserver.c
@@ -1,1 +1,17 @@
-hangman
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main(argc, *argc[]) {
+    
+    //socket
+    
+    //bind
+    
+    //listen
+    
+    //accept
+    
+    //read/write
+    
+    //close
+}
